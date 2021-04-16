d74eed62e8fee3825720bcb76e24fd0348d88275
Michael Sasonker <michaelsas1212@gmail.com>
2021-04-16 15:08:31 +0300

    tranfer  works

--- users/users.routes.js
+++ users/users.routes.js
@@ -151,6 +151,64 @@ router.get('/', (req,res) => res.status(200).json({ users: usersJSON.users }))
         }
     }
 })
+.put('/transfer',(req, res) => {
+    console.log(req.body);
+    const { srcPass, dstPass, amount } = req.body;
 
+    let resultSrc  = usersJSON.users.filter(user => user.passportId == srcPass);
+    let resultDst  = usersJSON.users.filter(user => user.passportId == dstPass);
+
+    if (!srcPass || !dstPass) {
+        return res.status(200).json({ error: "Please enter source and destination users!" });
+    }
+    else if (resultSrc.length === 0 || resultDst.length === 0) {
+        return res.status(200).json({ error: "No such passport id!" });
+    }
+    else if (isPositiveInt(amount) === false) {
+        return res.status(200).json({ error: "This amount of credit is not valid!" });
+    }
+    else if (srcPass == dstPass) {
+        return res.status(200).json({ error: "This is the SAME user!" });
+    }
+    else {
+        if (!resultSrc[0].isActive || !resultDst[0].isActive) {
+            return res.status(200).json({ error: "One of the users is NOT active!" });
+        }
+        else if ((resultSrc[0].credit === 0 && resultSrc[0].cash === 0) || (resultDst[0].credit === 0 && resultDst[0].cash === 0)) {
+            //tell the user to update credit!
+            return res.status(200).json({ error: "One of the users does NOT have enough money!" });
+        }
+        else if (resultSrc[0].cash - amount < (-1) * resultSrc[0].credit) {
+            return res.status(200).json({ error: "The source user does NOT have enough money!" });
+        }
+        else {
+            const userObjSrc = {
+                passportId: srcPass,
+                cash: resultSrc[0].cash - amount,
+                credit: resultSrc[0].credit,
+                isActive: resultSrc[0].isActive
+            }
+
+            const userObjDst = {
+                passportId: dstPass,
+                cash: resultDst[0].cash + amount,
+                credit: resultDst[0].credit,
+                isActive: resultDst[0].isActive
+            }
+    
+            let tempJSON = usersJSON;
+            let indxResSrc = tempJSON.users.findIndex((obj) => obj.passportId == srcPass);
+            let indxResDst = tempJSON.users.findIndex((obj) => obj.passportId == dstPass);
+
+            tempJSON.users.splice(indxResSrc, 1);
+            tempJSON.users.splice(indxResDst, 1);
+            tempJSON.users.push(userObjSrc);
+            tempJSON.users.push(userObjDst);
+
+            fs.writeFileSync('./users/users.json', JSON.stringify(tempJSON));
+            res.status(201).json({ message: "Action performed successfully!" });
+        }
+    }
+})
 
 module.exports = router;
