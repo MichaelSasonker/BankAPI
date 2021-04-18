9a83d088a56669f75fc70711ba9c8799f012de11
Michael Sasonker <michaelsas1212@gmail.com>
2021-04-18 11:59:30 +0300

    cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const usersRoutes = require('./users/users.routes');
 
 const app = express();
 const port = 8003;
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
